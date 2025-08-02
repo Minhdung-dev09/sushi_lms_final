78e7fe21c7a4a01c75604e980a0e1cfaa86f3f34
Doan Minh Dung <2121050986@student.humg.edu.vn>
2025-08-02 22:54:10 +0700

    Update axiosInstance.js

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://server-sushi-lms.onrender.com",
 });
 
 axiosInstance.interceptors.request.use(
